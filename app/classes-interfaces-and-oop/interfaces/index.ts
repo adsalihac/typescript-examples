a67f1d887d9ebef0b6b40017c0ece977f60fe65f
AHMED SALIH AC <36645339+adsalihac@users.noreply.github.com>
2023-04-05 15:14:51 +0530

    classes-interfaces-and-oop completed

---
+++ app/classes-interfaces-and-oop/interfaces/index.ts
@@ -0,0 +1,27 @@
+// abstract class Calender {
+//   constructor(public name: string) {}
+
+//   abstract addEvent(): void;
+//   abstract removeEvent(): void;
+// }
+
+interface Calender {
+  name: string;
+  addEvent(): void;
+  removeEvent(): void;
+}
+
+interface CloudCalender extends Calender {
+  sync(): void;
+}
+
+class GoogleCalender implements Calender {
+  constructor(public name: string) {}
+
+  addEvent(): void {
+    throw new Error("Method not implemented.");
+  }
+  removeEvent(): void {
+    throw new Error("Method not implemented.");
+  }
+}
